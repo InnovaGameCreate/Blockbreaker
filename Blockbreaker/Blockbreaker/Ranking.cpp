b9f58b4fa906e27df252bac19e9a9a34df1ba12f
俊樹 <@TOSHIKI>
2016-11-27 15:17:38 +0900

    ランキング機能

---
+++ Blockbreaker/Blockbreaker/Ranking.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "Ranking.h"
+
+
+Ranking::Ranking()
+{
+	clear_time = 0;
+
+}
+void Ranking::Add(int time, TCHAR name[])
+{
+	clear_time = time;
+	_stprintf(clear_name, _T("%s"), name);
+
+	/*配列にデータを保存*/
+	for (int i = 0; i < ARRAY_LENGTH(rank); i++) {
+		if (rank[i].enable == FALSE) {
+			rank[i].time = clear_time;
+			_stprintf(rank[i].name, _T("%s"), clear_name);
+			break;
+		}
+		else {
+			
+		};
+	}
+
+
+}
---
+++ Blockbreaker/Blockbreaker/Ranking.h
@@ -0,0 +1,17 @@
+#pragma once
+class Ranking
+{
+public:
+	Ranking();
+	void Add(int time, TCHAR name[]);
+private:
+	int clear_time;	//クリアした時間を保存する変数
+	TCHAR clear_name[16]; //クリアした人の名前を保存
+	struct rank_data {
+		int time;
+		TCHAR name[16];
+		int enable; //FALSEのとき無効
+	};
+	rank_data rank[40];
+};
+
